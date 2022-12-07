4bbab6ae5b5b0d37ce9fe52c5af50d18d44156d0
alvizek <avizek@flowwest.com>
2022-12-06 16:08:26 -0800

    initial push of data and metadata

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,4 +1,53 @@
 library(tidyverse)
+library(readxl)
 
+# TODO run is NA (is the join not working?)
+# TODO do we need the releaseID column? Probably not.
+# Decided to leave releaseID so it is really clear that this does not include recapture fish
+# TODO actualCount is still encoded. Need to add join
+catch <- read_xlsx(here::here("data-raw", "butte_catch_edi.xlsx"))
+write_csv(catch, here::here("data","butte_catch_edi.csv"))
 
-# save cleaned data to `data/`
+# TODO what are the units used for discharge, waterVel, waterTemp, turbidity
+# TODO are there other environmental we want to add?
+trap <- read_xlsx(here::here("data-raw", "butte_trap_edi.xlsx"))
+write_csv(trap, here::here("data", "butte_trap_edi.csv"))
+
+# TODO run is NA (is the join not working?)
+# TODO actualCount is still encoded. Need to add join
+recapture <- read_xlsx(here::here("data-raw", "butte_recapture_edi.xlsx"))
+write_csv(recapture, here::here("data","butte_recapture_edi.csv"))
+
+# TODO I do not think we need to upload this right now but will have it available for in the future
+release_fish <- read_xlsx(here::here("data-raw", "butte_releasefish_edi.xlsx"))
+write_csv(release_fish, here::here("data", "butte_releasefish_edi.csv"))
+
+# TODO we can remove the releaseID = 255 or releaseID = 0
+release <- read_xlsx(here::here("data-raw", "butte_release_edi.xlsx"))
+write_csv(release, here::here("data","butte_release_edi.csv"))
+
+# TODO write code that checks the data with lookups
+# flag if not in the lookup table
+
+# Create lookup table
+project <- 11
+fish_origin <- unique(catch$fishOrigin)
+lifestage <- unique(catch$lifeStage)
+mort <- unique(catch$mort)
+actual_count <- unique(catch$actualCount)
+site <- unique(catch$siteName)
+subsite <- unique(catch$subSiteName)
+visit_type <- unique(catch$visitType)
+fish_processed <- unique(trap$fishProcessed)
+trap_functioning <- unique(trap$trapFunctioning)
+include_catch <- unique(trap$includeCatch)
+marked_run <- unique(release$markedRun)
+marked_lifestage <- unique(release$markedLifeStage)
+marked_origin <- unique(release$markedFishOrigin)
+source_fish <- unique(release$sourceOfFishSite)
+release_site <- unique(release$releaseSite)
+release_subsite <- unique(release$releaseSubSite)
+mark_type <- unique(release$appliedMarkType)
+mark_color <- unique(release$appliedMarkColor)
+mark_position <- unique(release$appliedMarkPosition)
+
--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -1,5 +1,82 @@
-library(EDIutils)
+library(EMLaide)
 library(tidyverse)
+library(readxl)
+library(EML)
 
-
+datatable_metadata <-
+  dplyr::tibble(filepath = c(
+                             "data/butte_catch_edi.csv",
+                             "data/butte_repcature_edi.csv",
+                             "data/butte_release_edi.csv",
+                             "data/butte_trap_edi.csv"),
+                attribute_info = c("data-raw/metadata/butte_catch_metadata.xlsx",
+                                   "data-raw/metadata/butte_recapture_metadata.xlsx",
+                                   "data-raw/metadata/butte_release_metadata.xlsx",
+                                   "data-raw/metadata/butte_trap_metadata.xlsx"),
+                datatable_description = c("Daily catch",
+                                          "Recaptured catch",
+                                          "Release trial",
+                                          "Daily trap operations"),
+                datatable_url = paste0("https://raw.githubusercontent.com/FlowWest/jpe-butte-edi/main/data/",
+                                       c("catch.csv",
+                                         "recapture.csv",
+                                         "release.csv",
+                                         "trap.csv")))
 # save cleaned data to `data/`
+excel_path <- "data-raw/metadata/butte_metadata.xlsx"
+sheets <- readxl::excel_sheets(excel_path)
+metadata <- lapply(sheets, function(x) readxl::read_excel(excel_path, sheet = x))
+names(metadata) <- sheets
+
+abstract_docx <- "data-raw/metadata/abstract.docx"
+methods_docx <- "data-raw/metadata/methods.docx"
+
+#edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_USER_ID"), password = Sys.getenv("EDI_PASSWORD"))
+
+dataset <- list() %>%
+  add_pub_date() %>%
+  add_title(metadata$title) %>%
+  add_personnel(metadata$personnel) %>%
+  add_keyword_set(metadata$keyword_set) %>%
+  add_abstract(abstract_docx) %>%
+  add_license(metadata$license) %>%
+  add_method(methods_docx) %>%
+  add_maintenance(metadata$maintenance) %>%
+  add_project(metadata$funding) %>%
+  add_coverage(metadata$coverage, metadata$taxonomic_coverage) %>%
+  add_datatable(datatable_metadata)
+
+# GO through and check on all units
+# custom_units <- data.frame(id = c("number of fish", "rotations per minute", "rotations", "nephelometric turbidity units", "day"),
+#                            unitType = c("density", "dimensionless", "dimensionless", "dimensionless", "dimensionless"),
+#                            parentSI = c(NA, NA, NA, NA, NA),
+#                            multiplierToSI = c(NA, NA, NA, NA, NA),
+#                            description = c("Fish density in the enclosure, number of fish in total enclosure space",
+#                                            "Number of trap rotations in one minute",
+#                                            "Total rotations",
+#                                            "Nephelometric turbidity units, common unit for measuring turbidity",
+#                                            "The day sampling occured"))
+
+# unitList <- EML::set_unitList(custom_units)
+
+eml <- list(packageId = edi_number,
+            system = "EDI",
+            access = add_access(),
+            dataset = dataset
+            # additionalMetadata = list(metadata = list(unitList = unitList))
+)
+edi_number
+EML::write_eml(eml, paste0(edi_number, ".xml"))
+EML::eml_validate(paste0(edi_number, ".xml"))
+
+# EMLaide::evaluate_edi_package(Sys.getenv("user_ID"), Sys.getenv("password"), "edi.1047.1.xml")
+# EMLaide::upload_edi_package(Sys.getenv("user_ID"), Sys.getenv("password"), "edi.1047.1.xml")
+
+# The code below is for updating the eml number and will need to be implemented when
+# we move to automated updates
+# doc <- read_xml(paste0(edi_number, ".xml"))
+# edi_number<- data.frame(edi_number = doc %>% xml_attr("packageId"))
+# update_number <- edi_number %>%
+#   separate(edi_number, c("edi","package","version"), "\\.") %>%
+#   mutate(version = as.numeric(version) + 1)
+# edi_number <- paste0(update_number$edi, ".", update_number$package, ".", update_number$version)
