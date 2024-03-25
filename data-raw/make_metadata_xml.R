a154cd9554b226c9f8c8db4cc94ec7eb7c50f582
InigoP <inigop@rogers.com>
2024-03-25 11:54:36 -0700

    updated file path

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -99,8 +99,8 @@ EML::write_eml(eml, paste0(current_edi_number, ".xml"))
 message("EML Metadata generated")
 EMLaide::upload_edi_package(user_id = secret_edi_username,
                             password = secret_edi_password,
-                            # eml_file_path = paste0(getwd(), "/", current_edi_number, ".xml"),
-                            eml_file_path = paste0(getwd(), "/edi.1497.1.xml"),
+                            eml_file_path = paste0(getwd(), "/", current_edi_number, ".xml"),
+                            # eml_file_path = paste0(getwd(), "/edi.1497.1.xml"),
                             # existing_package_identifier = paste0("edi.",previous_edi_id, ".", previous_edi_ver, ".xml"),
                             environment = "staging")
 # The code below is for updating the eml number and will need to be implemented when
