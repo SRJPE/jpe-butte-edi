2d822180bc19e8e7f6b8ba6bec77cd60f9334cc7
InigoP <ipeng@flowwest.com>
2025-01-27 13:44:17 -0800

    make zip metadata

--- data-raw/bind_historic_data.R
+++ data-raw/bind_historic_data.R
@@ -1,58 +1,62 @@
 library(dplyr)
 library(readr)
+library(zip)
 
-append_historic_data <- function(historic_path, new_path){
+append_historic_data <- function(historic_path, new_path) {
+  # Temporary directories for extraction and writing
+  folder_path <- "data/butte.zip"
+  temp_dir <- tempdir()
+  temp_dir <- normalizePath(temp_dir, winslash = "/")
+  original_wd <- getwd()
+
+  # Unzip new_path
+  unzip(folder_path, exdir = temp_dir)
+  new_file <- file.path(temp_dir, basename(new_path))
+
+  # Load historic data
   historic_data <- readr::read_csv(historic_path)
-  if (new_path == "data/butte_catch.csv"){
-    new_data <- readr::read_csv(new_path)
-    full_data <- dplyr::bind_rows(historic_data, new_data)
-    col_order <- c("ProjectDescriptionID",
-                   "catchRawID",
-                   "trapVisitID",
-                   "commonName",
-                   "releaseID",
-                   "atCaptureRun",
-                   "fishOrigin",
-                   "lifeStage",
-                   "forkLength",
-                   "totalLength",
-                   "n",
-                   "visitTime",
-                   "visitType",
-                   "siteName",
-                   "subSiteName",
-                   "finalRun",
-                   "actualCount")
-    full_data <- full_data[,col_order]
-    write.csv(full_data, new_path, row.names = FALSE)
-      # filter(ProjectDescriptionID == 7) do i need to filter
-  }else{
-    new_data <- readr::read_csv(new_path)
-    full_data <- dplyr::bind_rows(historic_data, new_data)
-    col_order <- c("projectDescriptionID",
-                   "trapVisitID",
-                   "siteName",
-                   "subSiteName",
-                   "visitTime",
-                   "visitType",
-                   "fishProcessed",
-                   "trapFunctioning",
-                   "counterAtEnd",
-                   "rpmRevolutionsAtStart",
-                   "rpmRevolutionsAtEnd",
-                   "includeCatch",
-                   "discharge",
-                   "waterVel",
-                   "waterTemp",
-                   "turbidity")
-    full_data <- full_data[,col_order]
-    write.csv(full_data, new_path, row.names = FALSE)
+  # Load and filter new data
+  new_data <- if (grepl("butte_catch.csv", new_path)) {
+    readr::read_csv(new_file)
+  } else {
+    readr::read_csv(new_file)
   }
+  # Combine data
+  full_data <- dplyr::bind_rows(historic_data, new_data)
+  col_order <- c("ProjectDescriptionID",
+                 "catchRawID",
+                 "trapVisitID",
+                 "commonName",
+                 "releaseID",
+                 "atCaptureRun",
+                 "fishOrigin",
+                 "lifeStage",
+                 "forkLength",
+                 "totalLength",
+                 "n",
+                 "visitTime",
+                 "visitType",
+                 "siteName",
+                 "subSiteName",
+                 "finalRun",
+                 "actualCount")
+  full_data <- full_data[,col_order]
+  # Write updated data back to the temporary directory
+  write_csv(full_data, new_file)
+  setwd(temp_dir)
+  files_to_zip <- list.files(pattern = "^butte", recursive = TRUE)
+
+  zip(
+    zipfile = file.path(original_wd, folder_path),
+    files =  files_to_zip
+  )
+  setwd(original_wd)
+
 }
 
 path <- sort(c("butte_catch.csv", "butte_trap.csv"))
 full_historic_path <- paste0("data/historic_data/", path)
-full_new_data_path <- paste0("data/", path)
+full_new_data_path <- paste0("data/butte.zip/", path)
 
 mapply(append_historic_data, full_historic_path, full_new_data_path)
 
